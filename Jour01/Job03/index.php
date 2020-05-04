4bc29106ae9020c43f31996fe47f695a6a737f73
Léon Montagard <55874306+leon-montagard@users.noreply.github.com>
2020-05-04 11:03:56 +0200

    fini

---
+++ Jour01/Job03/index.php
@@ -0,0 +1,33 @@
+<?php
+$var1= true;
+$var2= 7;
+$var3= "Coucou les amis !";
+$var4= 7.362;
+?>
+<table>
+<tr>
+<td>Type</td>
+<td>Nom</td>
+<td>Valeur</td>
+</tr>
+<tr>
+<td>Booléen</td>
+<td> Var1</td>
+<td><?php echo $var1; ?></td>
+</tr>
+<tr>
+<td>entier</td>
+<td> Var2</td>
+<td><?php echo $var2; ?></td>
+</tr>
+<tr>
+<td>chaine de caractères</td>
+<td> Var3</td>
+<td><?php echo $var3; ?></td>
+</tr>
+<tr>
+<td>nombre à virgule flottante</td>
+<td> Var4</td>
+<td><?php echo $var4; ?></td>
+</tr>
+</table>
