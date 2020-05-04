fcf7f55637273229a7d5eeba2a320c68a08e4873
Léon Montagard <55874306+leon-montagard@users.noreply.github.com>
2020-05-04 10:36:31 +0200

    Add files via upload

---
+++ Jour01/Job01/index.php
@@ -0,0 +1,28 @@
+<?php 
+$str= " Hello LaPlateforme";
+$str2= "Vive";
+$str3= " !";
+
+echo $str2.$str.$str3;
+ ?>
+ 
+<br/>
+
+
+ <?php
+$val= 6;
+echo $val;
+echo "<br/>";
+$val= 6+4;
+echo $val;
+ ?>
+ 
+ <br/>
+ 
+
+ <?php
+$myBool= true;
+echo $myBool;
+$myBool= false;
+echo $myBool;
+ ?>
