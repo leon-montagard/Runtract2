10de95bac8a8d0f9e6f83a76f29348ad3885b3a3
Léon Montagard <55874306+leon-montagard@users.noreply.github.com>
2020-05-05 19:40:38 +0200

    fini

---
+++ jour02/job01/index.php
@@ -0,0 +1,19 @@
+<?php
+$nombre=0;
+
+while ( $nombre<= 40) {
+	$nombre++;
+	echo $nombre; 
+	echo "<br/>";
+}
+echo "<b><u>42</b></u>";
+
+$nombre=42;
+echo "<br/>";
+while ( $nombre<= 1337) {
+	$nombre++;
+	echo $nombre;
+	echo "<br/>";
+}
+
+  ?>
