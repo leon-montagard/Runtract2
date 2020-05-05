e7d28a6f54cddbd3c5c9070bf134c7913d74ed45
Léon Montagard <55874306+leon-montagard@users.noreply.github.com>
2020-05-05 20:12:57 +0200

    fini

---
+++ jour02/job02/index.php
@@ -0,0 +1,17 @@
+<?php  
+
+$nombre=0;
+
+while ( $nombre<= 1336) {
+	
+	if ($nombre==26 || $nombre==37 || $nombre=88 || $nombre=1111 || $nombre=3233) {
+		$nombre++;
+	}
+else{
+
+echo $nombre;
+echo "<br/>";
+$nombre++;
+}
+}
+?>
