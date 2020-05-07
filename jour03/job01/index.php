2f5e9ceb73f3be1e5e206975e9b5837dc84069d5
Léon Montagard <55874306+leon-montagard@users.noreply.github.com>
2020-05-07 15:54:41 +0200

    fini

---
+++ jour03/job01/index.php
@@ -0,0 +1,18 @@
+<?php
+$nombres["200"]["paire/impaire"]="Paire";
+$nombres["204"]["paire/impaire"]="Paire";
+$nombres["173"]["paire/impaire"]="impaire";
+$nombres["98"]["paire/impaire"]="Paire";
+$nombres["171"]["paire/impaire"]="impaire";
+$nombres["404"]["paire/impaire"]="Paire";
+$nombres["459"]["paire/impaire"]="impaire";
+
+echo "200 est ".$nombres["200"]["paire/impaire"]."<br/>";
+echo "204 est ".$nombres["204"]["paire/impaire"]."<br/>";
+echo "173 est ".$nombres["173"]["paire/impaire"]."<br/>";
+echo "98 est ".$nombres["98"]["paire/impaire"]."<br/>";
+echo "171 est ".$nombres["171"]["paire/impaire"]."<br/>";
+echo "404 est ".$nombres["404"]["paire/impaire"]."<br/>";
+echo "459 est ".$nombres["459"]["paire/impaire"]."<br/>";
+
+  ?>
